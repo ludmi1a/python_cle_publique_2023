9fdb80b2e9fab506104ced97343f914c69b41262
Ludmila Hadj Arab <ludmila.hadjarab@se.linedata.com>
2023-05-21 23:16:14 +0200

    composant cle cree

---
+++ composant_cle/component_cle.cpp
@@ -0,0 +1,59 @@
+#include <pybind11/pybind11.h>
+#include "micro-ecc/uECC.h"
+#include <string>
+#include <sstream>
+
+char version[]="1.0";
+
+char const* getVersion() {
+	return version;
+}
+
+class Cle
+{
+    public:
+        Cle(){}
+	
+	uint8_t castStringToUint8(const std::string& str) {
+		
+   		std::istringstream iss(str);
+    		int intValue;
+    		iss >> intValue;
+		
+    		if (intValue < 0 || intValue > 255) {}
+    		return static_cast<uint8_t>(intValue);
+	}
+	void initialize(std::string &pk){
+		privatekey = pk;
+		uint8_t a = castStringToUint8(privatekey);
+		uint8_t b = castStringToUint8(publickey);
+		uECC_Curve curve_256k1 = uECC_secp256k1();
+		uECC_make_key(&a,&b,curve_256k1);
+	}
+        const std::string &getPrivateKey() const {
+	       return privatekey;
+       	}
+        const std::string &getPublicKey() const {
+           return publickey;
+           }
+    private:
+        std::string privatekey;
+        std::string publickey;
+        
+};
+ 
+namespace py = pybind11;
+
+
+PYBIND11_MODULE(cle_component,greetings)
+{
+  greetings.doc() = "greeting_object 1.0";
+  greetings.def("getVersion", &getVersion, "a function returning the version");
+  
+   // bindings to Cle class
+    py::class_<Cle>(greetings, "Cle")
+        .def(py::init<>())
+	.def("initialize", &Cle::initialize)
+        .def("getPrivateKey", &Cle::getPrivateKey)
+        .def("getPublicKey", &Cle::getPublicKey);
+}
---
+++ composant_cle/test.py
@@ -0,0 +1,6 @@
+import component_cle
+
+macle = component_cle.Cle()
+macle.initialize("4b8e29b9b0dddd58a709edba7d6df6c07ebdaf5653e325114bc5318c238f87f0")
+print("Private key : " + macle.getPrivateKey())
+print("Public key : "+ macle.getPublicKey())
